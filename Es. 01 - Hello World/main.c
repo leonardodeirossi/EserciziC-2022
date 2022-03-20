9f7a7a1d5b79aa3f4fce8670843d5c5322f82c69
Leonardo Essam <leonardo.deirossi@icloud.com>
2022-03-20 14:19:04 +0100

    Upload primo esercizio.

---
+++ Es. 01 - Hello World/main.c
@@ -0,0 +1,9 @@
+// Direttiva per il pre-processore (stdio.h => standard I/O library)
+#include <stdio.h>
+
+// Funzione main non deve mai mancare e deve restituire un integer
+int main() {
+	printf("Hello World\n");
+	
+	return 0; // 0 => tutto OK (usato come standard)
+}
